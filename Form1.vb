938625c7d5beabee2b0f7902a69e7fcb7fcd7404
Dell-User <Dell-User@DESKTOP-4LNHG9G>
2023-10-05 12:01:04 +0200

    Add project files.

---
+++ Form1.vb
@@ -0,0 +1,25 @@
+﻿Imports System.Windows.Forms.VisualStyles.VisualStyleElement
+
+Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim grade As String
+        grade = TextBox1.Text
+        grade = Microsoft.VisualBasic.UCase(grade)
+
+        Select Case grade
+            Case "A"
+                TextBox2.Text = "High disticntion"
+
+            Case "B"
+                TextBox2.Text = "Disticition"
+            Case "C"
+                TextBox2.Text = "Credit"
+            Case "D"
+                TextBox2.Text = "PASS"
+            Case Else
+
+                TextBox2.Text = "FAIL"
+
+        End Select
+    End Sub
+End Class
